5d08671b21d333c6dc88808fc0f722452cd701cb
zhaole <lezhao1997@gmail.com>
2024-02-02 01:42:46 +0800

    2.9

--- Source/Aura/Private/Player/AuraPlayerController.cpp
+++ Source/Aura/Private/Player/AuraPlayerController.cpp
@@ -2,4 +2,37 @@
 
 
 #include "Player/AuraPlayerController.h"
+#include "EnhancedInputSubsystems.h"
 
+AAuraPlayerController::AAuraPlayerController()
+{
+	// 开启网络复制
+	bReplicates = true;
+}
+
+void AAuraPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// 断言输入配置
+	check(AuraContext);
+	// 获取本地玩家输入子系统的指针
+	UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer());
+	// 断言输入子系统
+	check(Subsystem);
+	// 将配置添加到子系统
+	Subsystem->AddMappingContext(AuraContext, 0);
+
+	// 显示鼠标
+	bShowMouseCursor = true;
+	// 鼠标光标为默认
+	DefaultMouseCursor = EMouseCursor::Default;
+
+	FInputModeGameAndUI InputModeData;
+	// 视口鼠标不锁定
+	InputModeData.SetLockMouseToViewportBehavior(EMouseLockMode::DoNotLock);
+	// 捕捉时不隐藏
+	InputModeData.SetHideCursorDuringCapture(false);
+	// 设置输入模式
+	SetInputMode(InputModeData);
+}
