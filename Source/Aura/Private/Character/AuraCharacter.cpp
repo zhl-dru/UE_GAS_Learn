60a089ac97d570b1b84dcb9538aa941e1e9fc8b3
zhaole <lezhao1997@gmail.com>
2024-05-05 22:13:08 +0800

    2.11

--- Source/Aura/Private/Character/AuraCharacter.cpp
+++ Source/Aura/Private/Character/AuraCharacter.cpp
@@ -3,3 +3,22 @@
 
 #include "Character/AuraCharacter.h"
 
+#include "GameFramework/CharacterMovementComponent.h"
+
+AAuraCharacter::AAuraCharacter()
+{
+	// 获取角色运动组件
+	// 旋转玩家朝向加速方向为 true
+	GetCharacterMovement()->bOrientRotationToMovement = true;
+	// 设置水平旋转速度为400度/秒
+	GetCharacterMovement()->RotationRate = FRotator(0.f, 400.f, 0.f);
+	// 限制角色移动在一个平面上
+	GetCharacterMovement()->bConstrainToPlane = true;
+	// 指定角色在开始移动时立即贴合到所在平面上
+	GetCharacterMovement()->bSnapToPlaneAtStart = true;
+
+	// 设置角色旋转不受控制器方向的影响
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationRoll = false;
+	bUseControllerRotationYaw = false;
+}
